bf78cb674ca0ea3ccee9eea4ba6823b7235f0b37
李沛池 <Godlike@God.local>
2015-09-17 19:45:02 +0800

    Initial Commit

---
+++ LOLInfomation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LOLInfomation
+//
+//  Created by 李沛池 on 15/9/17.
+//  Copyright (c) 2015年 LPC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
